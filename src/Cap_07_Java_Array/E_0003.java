0b3fcafa792589e44644598b80b637915da2ce3e
marsabatini <marco_rsabatini@hotmail.com>
2023-06-08 07:56:35 -0300

    V. 0.02

--- src/Cap_07_Java_Array/E_0003.java
+++ src/Cap_07_Java_Array/E_0003.java
@@ -1,4 +1,24 @@
 package Cap_07_Java_Array;
 
+// The program writes a grid using a two-dimensional array.
+
+import java.util.Arrays;
+
 public class E_0003 {
+
+    public static void main(String args[]){
+
+        String[][] grid = new String[10][10];
+
+        for(int i = 0; i < 10; i++){
+
+            for(int j = 0; j < 10; j++){
+
+                grid[i][j] = " - ";
+
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
