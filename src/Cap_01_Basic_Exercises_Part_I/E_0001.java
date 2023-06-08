25a83ff7ef5129c06b0a1683a80e302e1cffab10
marsabatini <marco_rsabatini@hotmail.com>
2023-06-08 08:02:32 -0300

    V. 0.02

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0001.java
@@ -0,0 +1,16 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // This code is simply presented by its author
+
+public class E_0001 {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello!!!");
+        System.out.println("I am Marco Sabatini and, here, we are going to do a series of" +
+                " exercises based on the proposals from the W3 Ressource site.");
+        System.out.println("\nDid you like it?");
+        System.out.println("The check out the website:\n\thttps://www.w3resource.com/java-exercises/");
+
+    }
+}
