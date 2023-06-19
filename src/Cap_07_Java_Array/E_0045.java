f0f1d75c5edafbb37b5f12a1076a0ba86b1b90a8
marsabatini <marco_rsabatini@hotmail.com>
2023-06-19 17:09:57 -0300

    V. 1.00

---
+++ src/Cap_07_Java_Array/E_0045.java
@@ -0,0 +1,28 @@
+package Cap_07_Java_Array;
+
+    // The program rotates cyclically a given array
+    // clockwise by one.
+
+import java.util.Arrays;
+
+public class E_0045 {
+
+    public static void main(String[] args){
+
+        int[] arr = { 10, 20, 30, 40, 50, 60 };
+
+        int[] arrNew = new int[arr.length];
+        int count = arr.length - 1;
+
+        System.out.println("The original array is: " + Arrays.toString(arr));
+
+        for(int i = 0; i < arr.length; i++){
+
+            arrNew[count] = arr[i];
+
+            count--;
+        }
+
+        System.out.println("The rotated array is: " +  Arrays.toString(arrNew));
+    }
+}
