9a2a21c1bb13a214b723755723a23e8beeb366c8
marsabatini <marco_rsabatini@hotmail.com>
2023-06-20 08:33:48 -0300

    V. 1.01

---
+++ src/Cap_07_Java_Array/E_0056.java
@@ -0,0 +1,35 @@
+package Cap_07_Java_Array;
+
+    // This program sorts a binary array in linear time
+
+import java.util.Arrays;
+
+public class E_0056 {
+
+    public static void main(String[] args){
+
+        int[] arr = new int[] { 0, 1, 1, 0, 1, 1, 0, 1, 0, 0 };
+
+        System.out.println("The original array is: " + Arrays.toString(arr));
+
+        sortBinary(arr);
+        System.out.println("The new original array is: " + Arrays.toString(arr));
+
+    }
+
+    static void sortBinary(int[] arr){
+
+        int x = 0;
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] == 0){
+                arr[x++] = 0;
+            }
+        }
+
+        for(int i = x; i < arr.length; i++){
+            arr[x++] = 1;
+        }
+    }
+}
