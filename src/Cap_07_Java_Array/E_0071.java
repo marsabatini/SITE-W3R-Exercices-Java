0b70b6814a5721ce84384283006d632b6e6aba0c
marsabatini <marco_rsabatini@hotmail.com>
2023-06-23 06:41:37 -0300

    V. 1.04

---
+++ src/Cap_07_Java_Array/E_0071.java
@@ -0,0 +1,31 @@
+package Cap_07_Java_Array;
+
+    // This program finds the largest number from a given list
+    // of non-negative integers.
+
+import java.util.Arrays;
+
+public class E_0071 {
+
+    public static void main(String[] args){
+
+        int[] arr = { 1, 2, 3, 0, 4, 6 };
+
+        System.out.println("The original array is: " + Arrays.toString(arr));
+        System.out.println("The largest number from array is: " + largestNumber(arr));
+
+    }
+
+    public static String largestNumber(int[] arr){
+
+        String[] arrayNumbers = Arrays.stream(arr)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+
+        Arrays.sort(arrayNumbers, (String str1, String str2) -> (str2 + str1).compareTo(str1 + str2));
+
+        return Arrays.stream(arrayNumbers)
+                .reduce((a, b) -> a.equals("0") ? b : a + b)
+                .get();
+    }
+}
