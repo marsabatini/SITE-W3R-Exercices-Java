1268e7ea47c8222e453dc72203d356e55eb6e0e0
marsabatini <marco_rsabatini@hotmail.com>
2023-06-24 07:38:00 -0300

    V. 1.05

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0002.java
@@ -0,0 +1,16 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // This simple program print the sum of two numbers
+
+
+public class E_0002 {
+
+
+    public static void main(String[] args){
+
+        int x = 74;
+        int y = 36;
+
+        System.out.println(x + " + " + y + " = " + (x + y));
+    }
+}
