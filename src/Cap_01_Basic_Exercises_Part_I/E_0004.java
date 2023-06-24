864625764254410f5ed02afda644b34a8ee811ae
marsabatini <marco_rsabatini@hotmail.com>
2023-06-24 07:40:46 -0300

    V. 1.05

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0004.java
@@ -0,0 +1,15 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // This program prints the results of various operations
+
+public class E_0004 {
+
+    public static void main(String[] args){
+
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+    }
+}
