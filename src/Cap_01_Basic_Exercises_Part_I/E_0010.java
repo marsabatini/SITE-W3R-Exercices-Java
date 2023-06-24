d6b42be72554b089be6eb595723bd02b5d31195a
marsabatini <marco_rsabatini@hotmail.com>
2023-06-24 08:13:50 -0300

    V. 1.05

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0010.java
@@ -0,0 +1,11 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // The program calculates a specified formula.
+
+public class E_0010 {
+
+    public static void main(String[] args){
+
+        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+    }
+}
---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0014.java
@@ -0,0 +1,5 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+public class E_0014
+{
+}
