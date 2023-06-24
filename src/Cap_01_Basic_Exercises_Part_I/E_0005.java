a0682c87f137d1906d3cdf69c16b48bd06466beb
marsabatini <marco_rsabatini@hotmail.com>
2023-06-24 07:45:35 -0300

    V. 1.05

--- src/Cap_01_Basic_Exercises_Part_I/E_0005.java
+++ src/Cap_01_Basic_Exercises_Part_I/E_0005.java
@@ -1,4 +1,27 @@
 package Cap_01_Basic_Exercises_Part_I;
 
+    // This code takes two numbers as input and
+    // it prints the product of them.
+
+import java.util.Scanner;
+
 public class E_0005 {
+
+    public static void main(String[] args){
+
+        float x, y;
+
+        Scanner enter = new Scanner(System.in);
+
+        System.out.print("Enter number one: ");
+        x = enter.nextFloat();
+
+        System.out.print("Enter number two: ");
+        y = enter.nextFloat();
+
+        float result = x * y;
+
+        System.out.printf("The product of %.2f and %.2f is: %.2f", x, y, result);
+
+    }
 }
