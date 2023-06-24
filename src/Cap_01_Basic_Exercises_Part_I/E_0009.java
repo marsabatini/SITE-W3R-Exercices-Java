c4e16062805a15c11311888cd2f3dfaad0c3ebaf
marsabatini <marco_rsabatini@hotmail.com>
2023-06-24 08:03:27 -0300

    V. 1.05

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0009.java
@@ -0,0 +1,12 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // The program computes the specified expressions
+    // and prints the output.
+
+public class E_0009 {
+
+    public static void main(String[] args){
+
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+}
