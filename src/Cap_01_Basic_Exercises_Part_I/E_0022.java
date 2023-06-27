bd92d2788fdfa8e86f8cb89bf4d35c16a90a2bee
marsabatini <marco_rsabatini@hotmail.com>
2023-06-27 08:42:30 -0300

    V. 1.06

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0022.java
@@ -0,0 +1,32 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // This program converts a binary number to a decimal number.
+
+import java.util.Scanner;
+
+public class E_0022 {
+
+    public static void main(String[] args){
+
+        long binNumber;
+        long decNumber = 0;
+        long j = 1;
+        long re;
+
+        Scanner enter = new Scanner(System.in);
+
+        System.out.print("Enter a binary number: ");
+        binNumber = enter.nextLong();
+
+        while(binNumber != 0){
+            re = binNumber % 10;
+            decNumber = decNumber + re * j;
+            j = j * 2;
+            binNumber = binNumber / 10;
+        }
+
+        System.out.println("Decimal number: " + decNumber);
+
+        enter.close();
+    }
+}
