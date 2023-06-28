6e2c127c80c541d847868830dd962c8ceb3eb64d
marsabatini <marco_rsabatini@hotmail.com>
2023-06-28 07:08:42 -0300

    V. 1.07

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0037.java
@@ -0,0 +1,21 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // This program reverses a string.
+
+import java.util.Scanner;
+
+public class E_0037 {
+
+    public static void main(String[] args){
+
+        Scanner enter = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        char[] c = enter.nextLine().toCharArray();
+
+        System.out.print("The reverse string is: ");
+        for(int i = c.length - 1; i >= 0; i--){
+            System.out.print(c[i]);
+        }
+    }
+}
