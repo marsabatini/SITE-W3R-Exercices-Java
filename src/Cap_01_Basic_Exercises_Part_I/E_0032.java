3a9408034449f2fd6929bcca2e40037f376a787b
marsabatini <marco_rsabatini@hotmail.com>
2023-06-28 06:09:25 -0300

    V. 1.07

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0032.java
@@ -0,0 +1,34 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // This program compares two numbers.
+
+import java.util.Scanner;
+
+public class E_0032 {
+
+    public static void main(String[] args){
+
+        int x, y;
+
+        Scanner enter = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        x = enter.nextInt();
+
+        System.out.print("Enter second number: ");
+        y = enter.nextInt();
+
+        if(x == y){
+            System.out.println("The numbers are equals.");
+            System.out.printf("%d = %d", x, y);
+        } else if( x > y){
+            System.out.println("The first number is bigger than second number.");
+            System.out.printf("%d > %d", x, y);
+        } else {
+            System.out.println("The second number is bigger than first number.");
+            System.out.printf("%d < %d", x, y);
+        }
+
+        enter.close();
+    }
+}
