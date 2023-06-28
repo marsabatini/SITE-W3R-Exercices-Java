702ece7c43aab7cb707c8f8326b99dc0303aac4c
marsabatini <marco_rsabatini@hotmail.com>
2023-06-28 06:50:08 -0300

    V. 1.07

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0035.java
@@ -0,0 +1,28 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // This program computes the area of a polygon.
+
+import java.util.Scanner;
+
+public class E_0035 {
+
+    public static void main(String[] args){
+
+        int numberSide;
+        double lenSide;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number of side: ");
+        numberSide = scanner.nextInt();
+
+        System.out.print("Enter a length of side: ");
+        lenSide = scanner.nextDouble();
+
+        double area = numberSide * (lenSide * lenSide)/(4 * Math.tan(Math.PI / numberSide));
+
+        System.out.println("The area of polygon is: " + area);
+
+        scanner.close();
+    }
+}
