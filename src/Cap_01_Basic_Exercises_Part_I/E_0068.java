c61180446652353e22f6f311fff13a7752647532
marsabatini <marco_rsabatini@hotmail.com>
2023-08-01 14:41:49 -0300

    V. 2.01

---
+++ src/Cap_01_Basic_Exercises_Part_I/E_0068.java
@@ -0,0 +1,18 @@
+package Cap_01_Basic_Exercises_Part_I;
+
+    // The code creates another string of 4 copies of the
+    // last 3 characters of the original string.
+    // Obs.: The original string length must be 3 or above.
+
+public class E_0068 {
+
+    public static void main(String args[]){
+
+        String originalStr = "Marco Sabatini";
+        int count = originalStr.length();
+        String newStr = originalStr.substring(count - 3, count);
+
+        System.out.println("The original string: " + originalStr);
+        System.out.println("The result is: " + newStr + newStr + newStr);
+    }
+}
